ad137b14f6ff3037fa348dbc2f7dc84414737201
v-kuper <vkuper7@gmail.com>
2025-03-08 00:45:15 +0300

    feat: add BMI struct

---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,9 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Vitali Kupratsevich on 8.03.25.
+//  Copyright © 2025 Angela Yu. All rights reserved.
+//
+
+import Foundation
---
+++ BMI Calculator/Models/CalculatorBrain.swift
@@ -0,0 +1,9 @@
+//
+//  CalculatorBrain.swift
+//  BMI Calculator
+//
+//  Created by Vitali Kupratsevich on 7.03.25.
+//  Copyright © 2025 Angela Yu. All rights reserved.
+//
+
+import Foundation
